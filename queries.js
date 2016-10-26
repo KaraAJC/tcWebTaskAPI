a8596e37f566f00556a1525253e02301e8dc4726
karaAJC <KaraAJC@gmail.com>
2016-10-26 13:39:01 -0500

    adds routes for cars

---
+++ queries.js
@@ -0,0 +1,50 @@
+// Database connection & config
+var promise = require('bluebird');
+
+var options = {
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://localhost:5432/cars';
+var db = pgp(connectionString);
+
+// Query Functions
+function getAllCars (req, res, next) {
+  db.any('select * from cars')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL cars'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function getACar(req, res, next) {
+  var carID = parseInt(req.params.id);
+  db.one('select * from cars where id = $1', carID)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved one car'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+module.exports = {
+  getAllCars: getAllCars,
+  getACar: getACar,
+  createACar: createACar,
+  updateACar: updateACar,
+  deleteACar: deleteACar
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../queries');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// GET CARS Index(read all)
+  router.get('/api/cars', db.getAllCars)
+
+// POST CARS Create
+  router. ('/api/cars/new', db.createACar)
+
+// GET CARS Show(read one)
+  router.get('/api/cars/:id', db.getACar)
+
+// PUT CARS Update
+  router. ('/api/cars/:id/update', db.updateACar)
+
+// DELETE CARS Destroy
+  router. ('/api/cars/', db.deleteACar)
 
 module.exports = router;
